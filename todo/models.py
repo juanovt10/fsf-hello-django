0b2bdba5ac789106e2b18fbed42f1795f4df0167
juanovt10 <>
2023-11-19 11:36:39 +0000

    Apply migrations and override django code to display the actual item names in the admin panel

--- todo/models.py
+++ todo/models.py
@@ -6,3 +6,8 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):
+        # this is to change the display name from item(1), item(2) to the actual
+        # name that we want to give to the item
+        return self.name
